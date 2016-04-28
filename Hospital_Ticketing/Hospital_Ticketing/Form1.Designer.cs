df265009a48066aa59e3c5e915aee4f72b583da7
geo kouvarakis <gkouv@sbzsystems.com>
2016-04-28 13:06:08 +0300

    Hospital_Ticketing

---
+++ Hospital_Ticketing/Hospital_Ticketing/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Hospital_Ticketing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLi = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tbuser = new System.Windows.Forms.TextBox();
+            this.tbpass = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLi
+            // 
+            this.btnLi.Location = new System.Drawing.Point(3, 186);
+            this.btnLi.Name = "btnLi";
+            this.btnLi.Size = new System.Drawing.Size(75, 23);
+            this.btnLi.TabIndex = 0;
+            this.btnLi.Text = "Log In";
+            this.btnLi.UseVisualStyleBackColor = true;
+            this.btnLi.Click += new System.EventHandler(this.btnLi_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(3, 227);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // tbuser
+            // 
+            this.tbuser.Location = new System.Drawing.Point(3, 53);
+            this.tbuser.Name = "tbuser";
+            this.tbuser.Size = new System.Drawing.Size(174, 20);
+            this.tbuser.TabIndex = 3;
+            this.tbuser.TextChanged += new System.EventHandler(this.tbuser_TextChanged);
+            // 
+            // tbpass
+            // 
+            this.tbpass.Location = new System.Drawing.Point(3, 104);
+            this.tbpass.Name = "tbpass";
+            this.tbpass.Size = new System.Drawing.Size(174, 20);
+            this.tbpass.TabIndex = 4;
+            this.tbpass.UseSystemPasswordChar = true;
+            this.tbpass.TextChanged += new System.EventHandler(this.tbpass_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbuser);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.tbpass);
+            this.panel1.Controls.Add(this.btnLi);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 280);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 279);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Log In";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLi;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox tbuser;
+        private System.Windows.Forms.TextBox tbpass;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hospital_Ticketing/Hospital_Ticketing/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospital_Ticketing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tbuser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbpass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLi_Click(object sender, EventArgs e)
+        {
+            string[] checkpass = { "Admin","User","Engineer" };
+            string[] checkusername= { "Admin", "User", "Engineer" };
+            bool temp = true;
+            for (int i = 0; i < 3; i++)
+            {
+
+                bool result = checkpass[i].Equals(tbpass.Text, StringComparison.Ordinal);
+                bool result1 = checkusername[i].Equals(tbuser.Text, StringComparison.Ordinal);
+                
+                
+                if (result == true & result1 == true)
+                {
+                    MessageBox.Show("" + i);
+                    temp = true;
+                    break;
+
+                }
+                else
+                {
+                    temp = false;
+                }
+                
+             
+                
+
+            }
+            if (temp == false)
+            {
+
+                MessageBox.Show("Invalid Username or Password");
+                tbpass.Clear();
+                tbuser.Clear();
+
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tbpass.Clear();
+            tbuser.Clear();
+        }
+    }
+}
